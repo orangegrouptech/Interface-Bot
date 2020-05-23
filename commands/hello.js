ce251f1cc4551bc7671c9381753ba37e3dd39fd4
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-05-23 02:25:44 -0700

    Update hello.js

--- commands/hello.js
+++ commands/hello.js
@@ -2,13 +2,13 @@ module.exports = {
   name: 'hi',
   aliases: ['hello', 'hey'],
   description: 'Says hello. What else would it do? :joy:',
-  usage: 'N/A',
   cooldown: 0,
+  staff:true,
 	execute(message, args, client) {
     try{
-   respond('', 'Hello! :wave:','', message.channel)
+   reply('', 'Hello! :wave:','', message.channel)
   }catch(error) {
-    respond('Error', 'Something went wrong.\n'+error+`\nMessage: ${message}\nArgs: ${args}\n`, message.channel)
+    reply('Error', error + `\n\nDebug info: \nMessage: ${message}\nArgs: ${args}\n`, message.channel)
     errorlog(error)
     console.log(error);
     }
