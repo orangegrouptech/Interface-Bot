fba48d7f26ef58766719089d5b1cbfa3e53e0de8
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-05-25 01:11:46 -0700

    Added command templates for new functions

---
+++ commands/templates/TEST_notificationsSignUpPrompt.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: "signup",
+    async execute(message, args, client) {
+        function responseReceived(result){
+          if(result == true){
+            reply('', 'Thank you for signing up!', '', message.channel)
+          }else if(result == false){
+            reply('', 'Hope you sign up next time!', '', message.channel)
+          }else if(result != true && result != false){
+            reply('', result, '', message.channel);
+          }
+        }
+        prompt('', 'Are you sure you want to sign up for news notifications?', '', message.channel, '', responseReceived)
+        }
+    }
