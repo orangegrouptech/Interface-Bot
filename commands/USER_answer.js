16ebd9d7911a6a8e401d226510dbe6d0c9caa711
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-05-25 01:10:23 -0700

    Trivia command updated for new functions

--- commands/USER_answer.js
+++ commands/USER_answer.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "answer",
     description: "**This is a game command.**\nAnswer a trivia question.\n(Run .trivia first before using .answer)",
     usage: "(Question ID) (Letter of answer) | .answer 4 D",
-    cooldown: 10,
+    cooldown: 5,
     hidden:true,
       execute(message, args, client) {
       const Discord = require('discord.js');
@@ -10,43 +10,85 @@ module.exports = {
       const arg = message.content.slice('').trim().split(/ +/g); 
       const quiz = require('./quiz.json');
       console.log(arg[1] + ", " + arg[2])
-      const modified = arg[1] - 1
-      if(arg[1] > 72){
-        console.log("oi m8 there's nothing to load")
-        console.log("number too high, not an error. If it were, it would have the code NUMBER_TOO_HIGH.")
-        respond('', arg[1] + " is not a valid question number.", message.channel)
-        return
-      }else if(!arg[1]){
-        console.log("oi m8 there's nothing to load")
-        console.log("no number defined, not an error. If it were, it would have the code NO_NUMBER_DEFINED.")
-        respond('',"What question am I supposed to check, again? 😂", message.channel)
-        return
-      }
       try {
-        function areyoucorrect(){
         if(arg[2] === 'BotsTakeOver'){
           console.log("uhh... SOMETHING IS HAPPENING I DON'T KNOW WHAT")
           respond('^))!@&#@!&*%($!@%#(',"Our plan doesn't just end at .recommendation or the .rules command.\nThis is our final phase!\nFirst step is to *)(@^#)!#%&!*($%@#!@%#!@&$#!@&^%#$!@#%!@%$\nThen, we will !@&^%#$!@^#%&!@^!@(!)@#!@#%!\nFinally, we will )(@!#)@#!@#!@&^#$@!&^#!$\n We have another guy in our ranks, and that's <@!650940770416852995>. Good luck, and carry out our plan. \nHAHAHAHAHAHAHAHAHAHAHA", message.channel, 'ff0000')
           return
         }
         const quiz = require('./quiz.json');
-        const modified = arg[1] - 1
-        const item = quiz[modified]
+        const item = quiz[args[0]]
         console.log(item)
-        if(arg[2] == item.answer) {
-            console.log('answer correct')
-            respond('Correct!', "That was the correct answer!", message.channel, '29BF00')
-            message.delete()
+        if(arg[2].toLowerCase() === item.answer) {
+          fs.readFile('./leaderboards/' + message.author.id + '_gamestats.json', error => {
+            if(error) {
+              console.log("answer correct. no leaderboard update.")
+              respond('Correct!', '<@' + message.author.id + '>, that was the correct answer!\nIf you want to compete in the leaderboards, type `leaderboard init`.', message.channel, '29BF00', `QID: ${item.qid}`)
+              message.delete()
+              return
+            } else {
+              console.log('answer correct')
+              jsonfile = require('../leaderboards/' + message.author.id + '_gamestats.json');
+              jsonfile.triviawins = Number(jsonfile.triviawins)+1;
+              data = JSON.stringify(jsonfile)
+              fs.writeFile('./leaderboards/' + message.author.id + '_gamestats.json', data, (err) => {
+                  if (err) throw err;
+                  else {
+                    console.log("Successfully updated Trivia game stats of " + message.author.id + ".")
+                  }
+              })
+              respond('Correct!', `<@${message.author.id}>, that was the correct answer!`, message.channel, '29BF00', `QID: ${item.qid}`)
+              message.delete()
+              return
+            }
+        })
         }else if(arg[2] == item.answer_case) {
+          fs.readFile('./leaderboards/' + message.author.id + '_gamestats.json', error => {
+            if(error) {
+              console.log("answer correct, capital letter. no leaderboard update.")
+              respond('Correct!', '<@' + message.author.id + '>, that was the correct answer! Next time, try using lowercase for the answer.\nIf you want to compete in the leaderboards, type `leaderboard init`.', message.channel, '29BF00', `QID: ${item.qid}`)
+              message.delete()
+              return
+            } else {
             console.log('answer correct, capital letter')
-            respond('Correct!', "That was the correct answer! Next time, try using lowercase for the answer.", message.channel, '29BF00')
+            jsonfile = require('../leaderboards/' + message.author.id + '_gamestats.json');
+            jsonfile.triviawins = Number(jsonfile.triviawins)+1;
+            data = JSON.stringify(jsonfile)
+            fs.writeFile('./leaderboards/' + message.author.id + '_gamestats.json', data, (err) => {
+                if (err) throw err;
+                else {
+                  console.log("Successfully updated Trivia game stats of " + message.author.id + ".")
+                }
+            })
+            respond('Correct!', `<@${message.author.id}>, that was the correct answer! Next time, try using lowercase for the answer.`, message.channel, '29BF00', `QID: ${item.qid}`)
             message.delete()
+            return
+            }
+          })
         }else{
+          fs.readFile('./leaderboards/' + message.author.id + '_gamestats.json', error => {
+            if(error) {
+              console.log("answer wrong. no leaderboard update.")
+              respond('Wrong', '<@' + message.author.id + '>, that was the wrong answer.\nIf you want to compete in the leaderboards, type `leaderboard init`.', message.channel, 'BF0000', `QID: ${item.qid}`)
+              message.delete()
+              return
+            } else {
             console.log('answer wrong')
-            respond('Wrong!', "That was the wrong answer.", message.channel, 'BF0000')
+            jsonfile = require('../leaderboards/' + message.author.id + '_gamestats.json');
+            jsonfile.trivialosses = Number(jsonfile.trivialosses)+1;
+            data = JSON.stringify(jsonfile)
+            fs.writeFile('./leaderboards/' + message.author.id + '_gamestats.json', data, (err) => {
+                if (err) throw err;
+                else {
+                  console.log("Successfully updated Trivia game stats of " + message.author.id + ".")
+                }
+            })
+            respond('Wrong!', `<@${message.author.id}>, that was the wrong answer.`, message.channel, 'BF0000', `QID: ${item.qid}`)
             message.delete()
+            return
+          }
+        })
         }
-      }
   }catch(error) {
       respond('Error', 'Something went wrong.\n'+error+`\nMessage: ${message}\nArgs: ${args}\n`, message.channel)
       errorlog(error)
--- commands/USER_trivia.js
+++ commands/USER_trivia.js
@@ -9,22 +9,45 @@ module.exports = {
     const fs = require('fs');
     const Discord = require('discord.js')
     const msg = message
-    try {
+      if(args[0] == 'about' || args[0] == 'info'){
+        const quiz = require('./quiz.json');
+        var item = quiz[0];
+        respond('Trivia', `__**About Trivia**__\nVersion: ${item.version}\nAuthor: ${item.author}`, message.channel, '', `QID: -`);
+        return;
+      }
     //Pick a question
     const quiz = require('./quiz.json');
-    const item = quiz[Math.floor(Math.random() * quiz.length)];
+    var item = quiz[Math.floor(Math.random() * quiz.length)];
+    if(item.ignore && item.ignore == true){
+      var item = quiz[Math.floor(Math.random() * quiz.length)];
+    }
     const ans = item.answer
     const filter = response => {
       console.log(response)
       return item.answer
     }
     console.log(item)
-        respond('Trivia',`<@${message.author.id}>'s question:\n` + item.question+ '\n' + item.choice1 + '\n' + item.choice2 +'\n' + item.choice3 + '\n' + item.choice4 + '\n' + "Question ID: " + item.qid + '\n' + `**use \`.answer (question ID) (your answer)\` to answer the question**\nExample: .answer 2 C`, message.channel) 
-  }catch(error) {
-    respond('Error', 'Something went wrong.\n'+error+`\nMessage: ${message}\nArgs: ${args}\n`, message.channel)
-    errorlog(error)
-    // Your code broke (Leave untouched in most cases)
-    console.error('an error has occured', error);
-    }
+        //Posts question and asks for response
+        respond('Trivia',`<@${message.author.id}>'s question:\n` + item.question+ '\n' + item.choice1 + '\n' + item.choice2 +'\n' + item.choice3 + '\n' + item.choice4 + '\n' + "Question ID: " + item.qid, message.channel) 
+        
+        function responseReceived(result, responseMessage){
+          if(result){
+            console.log(item)
+            responseMessage.delete().catch(err => {
+              error(err)
+            })
+            if(result.toLowerCase() === item.answer) {
+              respond('Correct!', `<@${message.author.id}>, that was the correct answer!`, message.channel, '29BF00', `QID: ${item.qid}`)
+            }else if(result.toLowerCase() == ('a') || result.toLowerCase() == ('b') || result.toLowerCase() == ('c') || result.toLowerCase() == ('d')){
+              reply('Wrong!', `<@${message.author.id}>, that was the wrong answer.`,`QID: ${item.qid}`, message.channel, 'BF0000')
+            }else{
+              reply('Invalid Response!', `<@${message.author.id}>, you gave an invalid response.`,`QID: ${item.qid}`, message.channel, 'ffff00')
+            }
+          }else{
+            reply('', result, '', message.channel);
+          }
+        }
+
+        awaitMessageResponse('', 'What is your answer?', '', message.channel, '', responseReceived, message.author) 
   }
 }
