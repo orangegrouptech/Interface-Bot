aeb9f552b00e01b7b40c9c4966c969b2c47f6da8
Daniel <46201432+DanielC-08@users.noreply.github.com>
2020-06-20 15:42:15 -0700

    Ability to skip file check on boot (through featuresConfig.json)

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 console.log('Loading. Please wait a moment.')
-fs = require('fs');
-
+const fs = require('fs');
+if(!fs.existsSync('./modules') || !fs.existsSync('./resources')){
+	return console.log('MISSING REQUIRED FILES!')
+}
+const featuresConfig = require('./resources/featuresConfig.json');
+const bot = require('./modules/bot');
+if(featuresConfig.bootFileCheck == true){
 returnFunction = function(returned, file){
 	if(returned == 'progress'){
 		console.log(`Passed: "${file}" was found.`)
 	}
 	if(returned == true){
 		console.log(`All files were successfully found.`)
-		bot = require('./modules/bot.js')
+		const bot = require('./modules/bot.js')
 		bot.execute()
 	}
 }
 const fileManager = require('./modules/fileManager.js')
 fileManager.verifyFiles(returnFunction)
+}else if(featuresConfig.bootFileCheck == false){
+	console.log('Automatic file check skipped.')
+	const bot = require('./modules/bot.js')
+	bot.execute()
+}else{
+	console.log(`Error: Missing entry in featuresConfig.json. (bootFileCheck)`)
+}
