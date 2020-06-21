9e1cc9b2ad2e5792a0c42e0f37e1b285ed975b40
Daniel <46201432+DanielC-08@users.noreply.github.com>
2020-06-21 16:02:47 -0700

    Create TEST_awaitResponseTimed.js

---
+++ commands/templates/TEST_awaitResponseTimed.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: "waitfor",
+    async execute(message, args, client) {
+        function responseReceived(result){
+          if(result != null){
+            reply('', result, 'You responded!', message.channel);
+          }else{
+            reply('', 'Something went wrong! Maybe you didn\'t respond?', '', message.channel);
+          }
+        }
+        awaitMessageResponse('', 'You said to wait for '+args[0], '', message.channel, '', responseReceived, message.author, Number.bind(args[0]))
+        }
+    }
