e35d59a39bd184ed5dd06e337d450de7df611af8
Daniel <46201432+DanielC-08@users.noreply.github.com>
2020-06-21 12:31:13 -0700

    debug commands moved to their own folder

---
+++ commands/debugCommands/TESTING_emuleave.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: 'emuleave',
+  aliases: ['leaveemu'],
+  description: 'This is for testing purposes only. Remove from release.',
+  usage: '',
+  cooldown: 0,
+  botmanager:true,
+  hidden:true,
+  staff:true,
+	execute(message, args, client) {
+    if(message.mentions.members.size == 0){
+      client.emit("guildMemberRemove", message.author)
+      respond('⬅️', `<@${message.author.id}> has "left" the server.`, message.channel)
+    }else{
+      client.emit("guildMemberRemove", message.mentions.members.first())
+      respond('⬅️', `<@${message.mentions.members.first().id}> has "left" the server.`, message.channel)
+    }
+  }}
