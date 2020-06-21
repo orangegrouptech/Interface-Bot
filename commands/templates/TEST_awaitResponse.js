8eddbef2bc5650c7647ad4d667202d87a7ffa776
Daniel <46201432+DanielC-08@users.noreply.github.com>
2020-06-21 12:34:04 -0700

    Update TEST_awaitResponse.js

--- commands/templates/TEST_awaitResponse.js
+++ commands/templates/TEST_awaitResponse.js
@@ -2,10 +2,10 @@ module.exports = {
   name: "repeatme",
     async execute(message, args, client) {
         function responseReceived(result){
-          if(result){
+          if(result != null){
             reply('', result, '', message.channel);
           }else{
-            reply('', result, '', message.channel);
+            reply('', 'Something went wrong! Maybe you didn\'t respond?', '', message.channel);
           }
         }
         awaitMessageResponse('', 'What do you want me to say?', '', message.channel, '', responseReceived, message.author)
