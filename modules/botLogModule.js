9a11ae55bc379aff77ff46a621331792ccf326eb
Daniel <46201432+DanielC-08@users.noreply.github.com>
2020-06-23 19:54:40 -0700

    Update botLogModule.js

--- modules/botLogModule.js
+++ modules/botLogModule.js
@@ -1,6 +1,7 @@
 const config = require('../resources/config.json')
 const featureConfig = require('../resources/featuresConfig.json')
 const Discord = require('discord.js')
+const fs = require('fs')
 module.exports = {
 	description:"Handles bot log events.",
 	errorLog(error, client){
@@ -14,6 +15,10 @@ module.exports = {
 				client.channels.cache
 				.get(`${config.botLog}`)
 				.send(errorReportEmbed);
+				if(fs.existsSync('./modules/debugLoggingModule.js')){
+					let debugModule = require('./debugLoggingModule.js')
+					return debugModule.log({action:"error", error:error})
+			}
 		}
 	},
 	botStart(client){
@@ -28,6 +33,13 @@ module.exports = {
 			.send(StartupEmbed);
 		}
 		console.log('Bot started successfully.')
+		switch (fs.existsSync('./modules/debugLoggingModule.js')){
+			case true:
+				let debugModule = require('./debugLoggingModule.js')
+				return debugModule.log({action:"startup-complete", error:null, note:"botLog"})
+			default:
+				return
+		}
 	},
 	debugLogging(text, client){
 			debugLoggingEmbed = new Discord.MessageEmbed()
@@ -38,5 +50,9 @@ module.exports = {
 				client.channels.cache
 				.get(`${config.debugChannel}`)
 				.send(debugLoggingEmbed);
+				if(fs.existsSync('./modules/debugLoggingModule.js')){
+					let debugModule = require('./debugLoggingModule.js')
+					return debugModule.log({action:"debugError", error:text, note:"botLog"})
+				}
 	}
 }
