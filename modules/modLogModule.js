d3bf8a47a737965ee332d06cc93496580060e716
Daniel <46201432+DanielC-08@users.noreply.github.com>
2020-06-23 19:54:25 -0700

    Update modLogModule.js

--- modules/modLogModule.js
+++ modules/modLogModule.js
@@ -1,9 +1,11 @@
 const config = require('../resources/config.json')
 const Discord = require('discord.js')
+const fs = require('fs')
 module.exports = {
     description:"Handles mod log events.",
     async messageEdit(oldMessage, newMessage, client){
 		if (newMessage.author.id === client.user.id) return;
+		if(newMessage.content == oldMessage.content)return;
 		let modLog = await client.channels.cache.get(config.modLog);
 		let messageUpdateEmbed = new Discord.MessageEmbed()
 		.setTitle("Message Edit")
@@ -12,6 +14,10 @@ module.exports = {
 		.addField("Original Message", oldMessage.content, true)
 		.addField("Updated Message", newMessage, true)
 		modLog.send(messageUpdateEmbed)
+		if(fs.existsSync('./modules/debugLoggingModule.js')){
+			let debugModule = require('./debugLoggingModule.js')
+			return debugModule.log({action:"messageEdit", error:null, note:"modLog"})
+	}
     },
     async messageDelete(message, client){
 		if (message.author.id === client.user.id) return;
@@ -22,5 +28,9 @@ module.exports = {
 		.setDescription(`ID: ${message.author.id}\nChannel: <#${message.channel.id}> (${message.channel.id})`)
 		.addField("Message", message.content, true)
 		modLog.send(messageDeleteEmbed)
+		if(fs.existsSync('./modules/debugLoggingModule.js')){
+			let debugModule = require('./debugLoggingModule.js')
+			return debugModule.log({action:"messageDelete", error:null, note:"modLog"})
+	}
     }
 }
